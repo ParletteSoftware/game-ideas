7fa3453643633c5d32d13155222cc1fd6e4b6e4d
Matt Parlette <matthew.parlette@gmail.com>
2013-07-03 15:51:00 -0400

    Added games controller and a view (new).

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,4 +1,6 @@
 Web::Application.routes.draw do
+  get "games/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -10,8 +12,7 @@ Web::Application.routes.draw do
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :games
 
   # Sample resource route with options:
   #   resources :products do
@@ -48,7 +49,7 @@ Web::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'games#index'
 
   # See how all your routes lay out with "rake routes"
 
